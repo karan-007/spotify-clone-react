3a9386ce8a2b7e13ab28d8672e81eb368c9bebab
karan chouhan <karan.chouhan@mountblue.tech>
2020-09-11 01:01:02 +0530

    albums added to search result

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -2,7 +2,7 @@ let fetchApi = url => new Promise((resolve, reject) => {
     fetch(url, {
         method: 'GET',
         headers: {
-            "Authorization": "Bearer BQDMEsf-KDJw5p0-FzHd8a1itMdIn_5azaB2RmoiZg_8MRFChCj8LaeY2tzip9dbVQEu3VOWwODiSxE7qQdAmFLJ7Rt-y01iiLjU1JVA60bLd7fBUmvvnlTWscAJ4mFZUKuWf3Pm5HVgx6yoP5Q_Us7CVyfG4lLtnfbaRXPEQj-z0a4zquhLTn-MjXSRgBiZLzYN5b1Wb4Kmf36QTPKforDtg_nhWo1sdPfO7pxUuTf99LFTec9GLqOGz4DhiD_lqjCBym0uKTzGS6vQunGgzyPcCUB8z_Lv"
+            "Authorization": "Bearer BQCdhMKiGOXZSW1epQQ7NB6IClmQj52EDR5cd5cXpUckdddMv3hJpzJ2GSld1BntG7Qpk3Q-3FGWb7wO75M3R2r-owRi3-ynBQPAXZgUdboRoS7uGNAytbywOT2L_5QPU2bIhxjHv1-Jq8DibEhgp_pOIF8CHaktUaTgdkTd8u0bPFt_cI8aIr8SE12dPZZbZ7oRL7_7l2zo2WUL5nKdADECOIo4LAKxZHCgFIUcnSI1UaFps6QJ5Mwrq5691A6Ddr08yrNymgDOUPlKZn8xsRKiBxQ1pcV7"
         }
     })
         .then(response => response.json())
