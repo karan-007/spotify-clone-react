f136a8b6cdddc4e5c6354eb42c596b40afbc0be9
karan chouhan <karan.chouhan@mountblue.tech>
2020-09-18 23:56:52 +0530

    footer player made

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,70 @@
-import React from 'react'
-import './Footer.css'
+import React, { useEffect, useState } from "react";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import "./Footer.css";
+import { Grid, Slider } from "@material-ui/core";
 
 function Footer() {
+    let playing = false;
+    let item = false;
     return (
-        <div className="footer-class">
-            <p>this is footer dsfdsfks</p>
+        <div className="footer">
+            <div className="footer__left">
+                {item ? (<img
+                    className="footer__albumLogo"
+                    src={item.album.images[0].url}
+                    alt={item.name}
+                />) : null}
+
+                {item ? (
+                    <div className="footer__songInfo">
+                        <h4>{item.name}</h4>
+                        <p>{item.artists.map((artist) => artist.name).join(", ")}</p>
+                    </div>
+                ) : (
+                        <div className="footer__songInfo">
+                            <h4>No song is playing</h4>
+                            <p>...</p>
+                        </div>
+                    )}
+            </div>
+
+            <div className="footer__center">
+                {playing ? (
+                    <PauseCircleOutlineIcon
+
+                        fontSize="large"
+                        className="footer__icon"
+                    />
+                ) : (
+                        <PlayCircleOutlineIcon
+
+                            fontSize="large"
+                            className="footer__icon"
+                        />
+                    )}
+            </div>
+            <div className="footer__right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider aria-labelledby="continuous-slider" />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Footer;
