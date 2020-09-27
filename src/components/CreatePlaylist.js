16babc6ee18d79aa2516e7f767c4ea4a06b8f709
harry.anto <harry.anto@mountblue.tech>
2020-09-27 21:55:04 +0530

    minor changes

--- src/components/CreatePlaylist.js
+++ src/components/CreatePlaylist.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
 import "../style/createPlaylist.css";
+import Playlist from './PlayList'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,12 +18,16 @@ function CreatePlaylist() {
   const classes = useStyles();
   return (
     <div className="body">
+        <div className="create">
       <h1>Create new playlist</h1>
       <form className={classes.root} noValidate autoComplete="off">
       {/* <TextField className="textField" id="standard-basic" size="normal" /> */}
       <input className="textField" type="text"/>
         <h3 onClick={handleClick} className="btn">Create</h3>
       </form>
+      </div>
+      <h4 style={{marginTop:'30px 0'}}>Playlists</h4>
+      {/* <Playlist/> */}
     </div>
   );
 }
