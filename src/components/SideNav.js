9823e882538f2f1b238422dc04bc9942475cfdbb
aquifzubair2 <aquif.zunair@mountblue.tech>
2020-10-01 13:41:39 +0530

    feat:implements the create play list modal form for creating playlist

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -8,16 +8,13 @@ import "../style/sideNav.css";
 import NavOption from "./NavOption";
 import { Link } from "react-router-dom";
 import fetchApi from "../fetchApi";
-import { Button, Modal } from "react-bootstrap";
-import axios from "../apiConfig/API";
-import {postApiWithAuth} from '../postApi'
+import CreatePlayList from './CreatePlaylist'
 
 function SideNav() {
   const [playlist, setPlaylist] = useState("");
   const [toggle, setToggle] = useState(false);
   const [error, setError] = useState("");
   const [modalShow, setModalShow] = useState(false);
-  const [playlistName, setPlaylistName] = useState("");
 
   useEffect(() => {
     fetchApi("/playlists")
@@ -48,53 +45,6 @@ function SideNav() {
     });
   }
 
-  function handleClick() {
-    postApiWithAuth("/playlists/add",{playlistName})
-    // axios({
-    //   method: "POST",
-    //   headers: {
-    //     Authorization: `Bearer ${localStorage.getItem("token")}`,
-    //   },
-    //   url: "/playlists/add",
-    //   data: { playlistName },
-    // })
-      .then((data) => console.log(data))
-      .then(() => setModalShow(false));
-
-    alert("platlist created!");
-  }
-
-  function handleChange(e){
-    console.log(e.target.value)
-    setPlaylistName(e.target.value)
-}
-
-  function MyVerticallyCenteredModal(props) {
-    return (
-      <Modal
-        {...props}
-        size="md"
-        className="modalTop"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header className="modalContainer">
-          <Modal.Title className="myModal" id="contained-modal-title-vcenter">
-            Create Playlist
-            <input
-              type="text"
-              className="textField1"
-              onChange={handleChange}
-            />
-            <h4 onClick={handleClick} className="create-btn">
-              Create
-            </h4>
-          </Modal.Title>
-        </Modal.Header>
-      </Modal>
-    );
-  }
-
   return (
     <div className="sideNav">
       <Link to="/">
@@ -104,28 +54,36 @@ function SideNav() {
           alt=""
         />
       </Link>
+
       <Link className="link-style" to="/">
         <NavOption Icon={HomeIcon} title="Home" />
       </Link>
+
       <Link className="link-style" to="/search">
         <NavOption Icon={SearchIcon} title="Search" />
       </Link>
+
       <Link className="link-style" to="/library">
         <NavOption Icon={LibraryMusicIcon} title="Your Library" />
       </Link>
+
       <br />
       <br />
       <strong className="sideNav-title">PLAYLISTS</strong>
+
       <Link className="link-style" onClick={() => setModalShow(true)}>
         <NavOption Icon={AddBoxIcon} title="Create Playlist" />
       </Link>
-      <MyVerticallyCenteredModal
+
+      <CreatePlayList
         show={modalShow}
         onHide={() => setModalShow(false)}
       />
+
       <Link className="link-style" to="/playlist/likedSongs">
         <NavOption Icon={FavoriteBorderIcon} title="Liked Songs" />
       </Link>
+      
       <hr />
       {playListData}
     </div>
